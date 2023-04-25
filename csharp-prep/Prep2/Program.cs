d24e1cb94584ed8e96a6a012801740a5521ffd44
Allena9 <austbrain@gmail.com>
2023-04-24 18:21:30 -0600

    Reads input percentage and outputs in a string.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+
+        Console.WriteLine("What is your grade percentage?");
+        string gradeString = Console.ReadLine();
+        int percentage = int.Parse(gradeString);
+
+        if(percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if(percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if(percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if(percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You received a {letter}.");
+
+        if(percentage >= 70)
+        {
+            Console.WriteLine("Congragulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time.");
+        }
     }
 }
