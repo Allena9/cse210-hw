b5a1f8a3868a1668e020b345e1343b8efe535352
Allena9 <austbrain@gmail.com>
2023-04-26 12:51:19 -0600

    Displays name and square of fav number.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name;
+        int favNum;
+        int squareNumber;
+
+        DisplayWelcome();
+        name = PromptUserName();
+        favNum = PromptUserNumber();
+        squareNumber = SquareNumber(favNum);
+        DisplayResult(name, squareNumber);
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        string input;
+        Console.WriteLine("Please enter your name: ");
+        input = Console.ReadLine();
+        return input;
+    }
+
+    static int PromptUserNumber()
+    {
+        int input;
+        Console.WriteLine("Please enter your favorite number: ");
+        input = int.Parse(Console.ReadLine());
+        return input;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int output = number * number;
+        return output;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
+
 }
