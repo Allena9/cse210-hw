90f695ca6a0a325b73331155d8aa51f899fc2b6a
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-06-14 19:20:48 -0700

    Outputs received name in James Bond format.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string first;
+        string last;
+
+        //Prompts for first and last name. Saves to corisponding variables. 
+        Console.Write("What is you first name? ");
+        first = Console.ReadLine();
+        Console.Write("Whats your last name? ");
+        last = Console.ReadLine();
+
+        //Dispalys name in James Bond format. 
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
