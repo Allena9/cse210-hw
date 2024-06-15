46d96400ba18e52bd72330caefc7422c653a2396
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-06-14 21:14:01 -0700

    Takes a list of numbers and displays the sum, average, and largest Number of the list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int temp = -1;
+        int sum = 0; 
+        double average;
+        int largestNum = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while(temp != 0)
+        {
+            Console.Write("Enter number: ");
+            temp = int.Parse(Console.ReadLine());
+            numbers.Add(temp);
+
+        }
+        
+        //Count the sum, and find the largest number.
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largestNum)
+            {
+                largestNum = number;
+            }
+        }
+
+        //Calculate the average.
+        average = Convert.ToDouble(sum) / Convert.ToDouble(numbers.Count);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNum}");
+
     }
 }
