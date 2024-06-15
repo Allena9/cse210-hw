d9d34b2c38681719bfa1dc2c2731cf728aefd8ef
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-06-14 21:39:46 -0700

    Takes a user name and number. Sqaures the number then returns with user name.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name;
+        int number;
+        int square;
+
+        DisplayWelcome();
+        name = PromptsUserName();
+        number = PromptsUserNumber();
+        square = SquareNumber(number);
+        DisplayResult(name, square);
+    }
+
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptsUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    static int PromptsUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNum = int.Parse(Console.ReadLine());
+
+        return userNum;
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
     }
 }
