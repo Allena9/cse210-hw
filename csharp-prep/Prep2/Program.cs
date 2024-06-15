b89faa2e43bee451f1621f440b09d03e335f1e0d
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-06-14 20:13:04 -0700

    Acceptes a grade as a percent. Returns letter grade with message.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade = "";
+
+        //Asks user for grade and stores it as an integer.
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        //Sort what letter grade the user got. 
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+
+        //Display letter grade.
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        //Determine if user passed.
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. You'll do better next time!");
+        }
     }
 }
