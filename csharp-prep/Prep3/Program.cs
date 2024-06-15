9f9560567c72482e2e006e0e657c669a2a6017b3
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-06-14 20:43:11 -0700

    Creates random number, prompts user to guess number till correct.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playGame = "yes";
+
+        while (playGame == "yes")
+        {
+            int guess;
+            int counter = 0;
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 100);
+
+            do
+            {
+                counter++;
+
+                Console.Write("What is your guess? ");
+                _ = int.TryParse(Console.ReadLine(), out guess);
+
+                if (magicNum == guess)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (magicNum > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNum < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (magicNum != guess);
+
+            Console.WriteLine($"Number of guesses: {counter}");
+            
+            Console.Write("Would you like to play again? (yes/no) ");
+            playGame = Console.ReadLine();
+        }
     }
 }
