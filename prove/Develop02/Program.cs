c232f009f17f9448e5bd29ab4aa15b68650fad2c
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-06-26 16:03:15 -0700

    Runs menu for interacting with a journal.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int counter = 0;
+        Journal journal = new Journal();
+
+        //Displays menu and prompts user for selection
+        while(counter != 5)
+        {
+
+            Console.WriteLine("Please select one of the following choices: ");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            
+            counter = Int32.Parse(Console.ReadLine());
+
+            if (counter == 1)
+            {
+                journal.AddEntry();
+                counter = 0;
+            }
+            else if (counter == 2)
+            {
+                journal.DisplayAll();
+                counter = 0;
+            }
+            else if (counter == 3)
+            {
+                journal.LoadFromFile();
+                counter = 0;
+            }
+            else if (counter == 4)
+            {
+                journal.SaveToFile();
+                counter = 0;
+            }
+
+        }
     }
 }
