18cf34aaa30fa2a869f8555c5271e6ba2c454c9d
Allena9 <131019371+Allena9@users.noreply.github.com>
2024-07-22 16:06:30 -0700

    Displays color and area of each shape.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = [];
+
+        Square square = new Square("blue", 2.0);
+        Rectangle rectangle = new Rectangle("red", 2.0, 4.0);
+        Circle circle = new Circle("orange", 3.0);
+
+        list.Add(square);
+        list.Add(rectangle);
+        list.Add(circle);
+
+        foreach (Shape shape in list)
+        {
+            Console.WriteLine($"{shape.GetColor()}");
+            Console.WriteLine($"{shape.GetArea()}");
+        }
     }
 }
